231333e48595f365bcf313fa66e0aeddd1ec6e4a
Mono <ferch5003@gmail.com>
2024-05-01 11:07:32 -0500

    feat: Add Mailer on main for sending emails

--- go-micro/mail-service/cmd/api/main.go
+++ go-micro/mail-service/cmd/api/main.go
@@ -3,15 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 const _webPort = "80"
 
 type Config struct {
+	Mailer Mail
 }
 
 func main() {
-	app := Config{}
+	app := Config{
+		Mailer: createMail(),
+	}
 
 	log.Println("Starting Mail Service on port", _webPort)
 
@@ -23,4 +28,24 @@ func main() {
 	if err := srv.ListenAndServe(); err != nil {
 		log.Panicln(err)
 	}
+}
+
+func createMail() Mail {
+	port, err := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	if err != nil {
+		log.Println(err)
+	}
+
+	m := Mail{
+		Domain:      os.Getenv("MAIL_DOMAIN"),
+		Host:        os.Getenv("MAIL_HOST"),
+		Port:        port,
+		Username:    os.Getenv("MAIL_USERNAME"),
+		Password:    os.Getenv("MAIL_PASSWORD"),
+		Encryption:  os.Getenv("MAIL_ENCRYPTION"),
+		FromAddress: os.Getenv("MAIL_ADDRESS"),
+		FromName:    os.Getenv("MAIL_NAME"),
+	}
+
+	return m
 }
